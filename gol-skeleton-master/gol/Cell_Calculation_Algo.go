45acadd38e696eb63a3ed15adfe12f3abf2d6979
Henry Shin <bg22915@bristol.ac.uk>
2024-11-10 18:57:12 +0000

    Final commit

--- gol-skeleton-master/gol/Cell_Calculation_Algo.go
+++ gol-skeleton-master/gol/Cell_Calculation_Algo.go
@@ -4,29 +4,75 @@ import (
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
-func mod(a, b int) int {
-	return (a%b + b) % b
-}
+// CPU profiling (Before)
 
+//func mod(a, b int) int {
+//	return (a%b + b) % b
+//}
+//
+//func calculateNeighbours(height, width int, world [][]byte, y int, x int) int {
+//
+//	h := height
+//	w := width
+//	noOfNeighbours := 0
+//
+//	neighbour := []byte{
+//		world[mod(y+1, h)][mod(x, w)],
+//		world[mod(y+1, h)][mod(x+1, w)],
+//		world[mod(y, h)][mod(x+1, w)],
+//		world[mod(y-1, h)][mod(x+1, w)],
+//		world[mod(y-1, h)][mod(x, w)],
+//		world[mod(y-1, h)][mod(x-1, w)],
+//		world[mod(y, h)][mod(x-1, w)],
+//		world[mod(y+1, h)][mod(x-1, w)],
+//	}
+//
+//	for i := 0; i < 8; i++ {
+//		if neighbour[i] == 255 {
+//			noOfNeighbours++
+//		}
+//	}
+//
+//	return noOfNeighbours
+//}
+
+// CPU profiling (After)
 func calculateNeighbours(height, width int, world [][]byte, y int, x int) int {
 
 	h := height
 	w := width
 	noOfNeighbours := 0
 
-	neighbour := []byte{
-		world[mod(y+1, h)][mod(x, w)],
-		world[mod(y+1, h)][mod(x+1, w)],
-		world[mod(y, h)][mod(x+1, w)],
-		world[mod(y-1, h)][mod(x+1, w)],
-		world[mod(y-1, h)][mod(x, w)],
-		world[mod(y-1, h)][mod(x-1, w)],
-		world[mod(y, h)][mod(x-1, w)],
-		world[mod(y+1, h)][mod(x-1, w)],
+	// Pre-calculate and store the coordinates of adjacent cells
+	neighbors := [8][2]int{
+		{y + 1, x},     // Below
+		{y + 1, x + 1}, // Bottom-right
+		{y, x + 1},     // Right
+		{y - 1, x + 1}, // Top-right
+		{y - 1, x},     // Above
+		{y - 1, x - 1}, // Top-left
+		{y, x - 1},     // Left
+		{y + 1, x - 1}, // Bottom-left
 	}
 
-	for i := 0; i < 8; i++ {
-		if neighbour[i] == 255 {
+	for _, coord := range neighbors {
+		ny, nx := coord[0], coord[1]
+
+		// Handle Y-coordinate wrap-around
+		if ny < 0 {
+			ny = h - 1
+		} else if ny >= h {
+			ny = 0
+		}
+
+		// Handle X-coordinate wrap-around
+		if nx < 0 {
+			nx = w - 1
+		} else if nx >= w {
+			nx = 0
+		}
+
+		if world[ny][nx] == 255 {
 			noOfNeighbours++
 		}
 	}
